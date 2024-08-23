5a505a9d7080801956c11b7d09ffc79759dffddc
Ece Kurşun <67229702+ecekursun@users.noreply.github.com>
2024-08-23 21:21:35 +0300

    Add files via upload

---
+++ day1a_aritmetikOperatorlerVeKisaltmalar.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// Daire alanı hesaplama
+
+let pi = 3.14
+let yariCap = 2.0
+let alan = pi * yariCap * yariCap
+
+print(alan)
+
+// F bulma
+
+var m = 12.5
+var a = 10.0
+var f = m * a
+
+print(f)
+
+// yer değiştirme hesaplama
+var v = 12.7
+var v0 = 23.56
+var t = 3.5
+
+var x1 = ((v + v0) / 2) * t
+
+print(x1)
+
+var y = 10
+y = y + 2 // kısayolu y += 2   
+
