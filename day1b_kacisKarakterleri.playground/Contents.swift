ad785c7a824e263c4f2b80458f8bdd19814d0ec3
Ece Kurşun <67229702+ecekursun@users.noreply.github.com>
2024-08-23 21:35:41 +0300

    Add files via upload
    day 1 b uploaded

---
+++ day1b_kacisKarakterleri.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let yazi = "Mehmet \"Nasılsın\" dedi" // yorum satırı aynı satırda olabilir
+
+print(yazi)
+
+var yazi1 = "Merhaba bu \"ios\"\n\teğitiminde \\swift\\ dilini öğreniyoruz"
+
+print(yazi1)
