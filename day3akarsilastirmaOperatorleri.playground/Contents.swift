39e7c1c6ce8e07a9c14adccebd6df2d7a62f40d7
Ece Kurşun <67229702+ecekursun@users.noreply.github.com>
2024-08-25 22:48:32 +0300

    Add files via upload

---
+++ day3akarsilastirmaOperatorleri.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var s1 = 60
+var s2 = 50
+
+var y1 = 70
+var y2 = 80
+
+print(s1==s2) // false
+print(s1>s2) // true
+print(s1>=s2) // true
+print(s1<s2) // false
+print(s1 != s2) // true
+
+print(s1>s2 || y1>y2) // (or)true
+
+print(s1>s2 && y1>y2) // (and) false
+
