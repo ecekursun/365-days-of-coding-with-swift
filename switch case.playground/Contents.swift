5512b28ecc3fcdb8a823813cb22421ffc4cb0a59
Ece Kurşun <67229702+ecekursun@users.noreply.github.com>
2024-08-26 21:58:49 +0300

    Add files via upload

---
+++ switch case.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let gun = 9
+
+switch gun {
+case 1:
+    print("pazartesi")
+case 2:
+    print("salı")
+case 3:
+    print("çarşamba")
+case 4:
+    print("perşembe")
+case 5:
+    print("cuma")
+case 6:
+    print("cumartesi")
+case 7:
+    print("pazar")
+default:
+    print("böyle bir gün yok")
+}
