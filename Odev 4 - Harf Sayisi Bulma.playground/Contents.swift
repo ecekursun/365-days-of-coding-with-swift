805a4a4384f38c7a4408c37babe8a62f9b9d2927
Ece Kurşun <67229702+ecekursun@users.noreply.github.com>
2024-09-06 23:32:23 +0300

    Update Contents.swift

--- Odev 4 - Harf Sayisi Bulma.playground/Contents.swift
+++ Odev 4 - Harf Sayisi Bulma.playground/Contents.swift
@@ -2,4 +2,20 @@ import UIKit
 
 // Parametre olarak girilen kelime ve harf için harfin kelime içinde kaç adet olduğunu gösteren bir metod yazınız.
 
+class Odev4 {
+    func kelimeAdetiBul(kelime:String,harf:Character) {
+        var sonuc = 0
+        
+        for k in kelime {
+            if k == harf {
+                sonuc = sonuc + 1
+            }
+        }
+        print("Harf adeti: \(sonuc)")
+    }
+}
+
+let o4 = Odev4()
+
+o4.kelimeAdetiBul(kelime: "ankara", harf: "a")
 
