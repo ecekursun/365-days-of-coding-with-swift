3ce613af4f36798d66d262cb0b1d613121a3d4b4
Ece Kurşun <67229702+ecekursun@users.noreply.github.com>
2024-09-08 22:54:01 +0300

    Add files via upload

---
+++ Static Degisken ve Metodlar.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class Asinifi {
+    static var x = 10
+    
+    static func metod() {
+        print("Merhaba")
+    }
+}
+
+print(Asinifi.x) // direkt erişebiliyorsun
+Asinifi.metod() // Merhaba
