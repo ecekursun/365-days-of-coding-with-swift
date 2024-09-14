0836c13507c867625a7adf38aa232330cd105eb0
Ece Kurşun <67229702+ecekursun@users.noreply.github.com>
2024-09-14 20:50:57 +0300

    Add files via upload

---
+++ Nesne tabanli listeleme - dictionary - ogrenci uygulama.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Ogrenci {
+    var no:Int?
+    var ad:String?
+    var sinif:String?
+    
+    init(no: Int?, ad: String?, sinif: String?) {
+        self.no = no
+        self.ad = ad
+        self.sinif = sinif
+    }
+}
